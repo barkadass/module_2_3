fbe1b12c26901267c0874798ee37359a895a1f46
barkadass <barkadas@mail.ru>
2024-08-01 21:22:44 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,21 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while len(my_list) > index:
+    if my_list[index] < 0:
+        break
+    elif my_list[index] > 0:
+        print(my_list[index])
+    index += 1
+
+
+
+
+# my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+# index = -1
+# while len(my_list) > index:
+#     if my_list[index] < 0:
+#         break
+#     elif my_list[index] > 0:
+#         print(my_list[index])
+#     index += 1
+# Так больше получается
